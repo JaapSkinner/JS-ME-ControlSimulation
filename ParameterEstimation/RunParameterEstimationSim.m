d14ef2a38189d46b1d755ecedac71af99d00e9fc
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2025-06-17 16:16:25 +1200

    Fixed Running Scripts and Video Generation

---
+++ ParameterEstimation/RunParameterEstimationSim.m
@@ -0,0 +1,66 @@
+    % RunParameterEstimationSim.m
+    clear; clc;
+    
+    % Run the base script to load and initialize everything
+    run('ParameterEstimationBase.m');
+    addpath(submodulePath);
+    
+    
+    
+    
+    %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    %% Create test tag and file name
+    testCase = 'estimation';
+    tStr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
+    outputFolder = fullfile(projectRoot, 'Results', 'ParameterEstimation');
+    outputFile = sprintf('%s_%s_%s', testCase, uavType, tStr);
+    
+    
+    
+    
+    
+    
+    %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    %% Deactivate initial states
+    set_param(modelName, 'LoadInitialState', 'off' );
+    set_param( modelName, 'LoadExternalInput', 'off' );
+    
+    % Mean wind for log
+    UMean = mean(windInput.Data(:,1));
+    
+    
+    
+    
+    
+    %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    %% 4) CUSTOM CODE
+    % pUT ANY SPECIAL CONDITIONS FOR YOUR SIMULATION, SUCH AS POSITION OR
+    % ATTITUDE SETPOINTS, HERE...
+    
+    
+    
+    
+    
+    
+    
+    
+    %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    %% RUN SIMULATION
+    simIn = Simulink.SimulationInput(modelName);
+    simIn = simIn.setVariable('windInput', windInput);
+    simIn = simIn.setVariable('uavType', uavType);
+    simIn = simIn.setVariable('windFile', windFile);
+    simIn = simIn.setVariable('Simulation', Simulation);
+    simIn = simIn.setVariable('Uav', Uav);
+    simIn = simIn.setVariable('Motor', Motor);
+    simIn = simIn.setVariable('Initial', Initial);
+    
+    disp('Simulation runs:')
+    fprintf('\t[%d/%d] wind file: % 18s, Umean = %5.2f m/s\n', 1, 1, windFile, UMean);
+    
+    simOut = sim(simIn);
+    
+    if ~isfolder(outputFolder)
+        mkdir(outputFolder)
+    end
+    save(fullfile(outputFolder, outputFile), 'simIn', 'simOut', 'Uav', 'Motor', 'windInput', 'windFile');
